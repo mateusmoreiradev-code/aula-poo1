4704965514a1be919652089fc4abcb2e65460629
mateusmoreiradev-code <mateus.moreira.dev@gmail.com>
2026-05-13 22:19:07 -0300

    feat: aula 13/05 criação de menu main.

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,111 @@
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
+import static java.lang.Integer.parseInt;
 
 public class Main {
     private static final Scanner sc = new Scanner(System.in);
-    private List<Cliente> cliente = new ArrayList<>();
+    private List<Cliente> clientes = new ArrayList<>();
+    private static final List<Categoria> categorias = new ArrayList<>();
+    private static final List<Entregador> entregadores = new ArrayList<>();
+    private static final List<Pedido> pedidos = new ArrayList<>();
+    private static final List<Restaurante> restaurantes = new ArrayList<>();
 
     private static int idUsuario = 1;
 
     public static void main(String[] args){
-        System.out.println("------Cadastro de Cliente------");
-        System.out.println("Digite o nome do cliente: ");
-        String nome = sc.nextLine();
+        boolean executando = true;
 
-        System.out.println("Digite o CPF: ");
-        String CPF = sc.nextLine();
+        while(executando) {
+            System.out.println("- - - - - - - - - - - - -");
+            System.out.println("   Sistema de Delivery   ");
+            System.out.println("- - - - - - - - - - - - -");
+            System.out.println("1. Area de ADM");
+            System.out.println("2. Area de Cliente");
+            System.out.println("3. Area de Entregador");
+            System.out.println("0. Sair");
+            System.out.println("Escolha uma opção: ");
 
-        System.out.println("Digite o e-mail: ");
-        String email = sc.nextLine();
+            int opcao = parseInt(sc.nextLine());
 
-        System.out.println("Digite a senha: ");
-        String senha = sc.nextLine();
+            switch (opcao) {
+                case 1:
+                    menuADM();
+                    break;
+                case 2:
+                    menuCliente();
+                    break;
+                case 3:
+                    menuEntregador();
+                    break;
+                case 0:
+                    executando = false;
+                    System.out.println("Encerrando o sistema");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+            sc.close();
+        }
+    }
+    public static void menuADM(){
+        boolean voltar = false;
 
-        System.out.println("Digite o telefone: ");
-        String telefone = sc.nextLine();
+        while(!voltar){
+            System.out.println("- - - - - - - - - - - - -");
+            System.out.println("  Area do Administrador  ");
+            System.out.println("- - - - - - - - - - - - -");
+            System.out.println("1. Cadastrar categoria");
+            System.out.println("2. Cadastrar restaurante");
+            System.out.println("3. Cadastrar produto no restaurante");
+            System.out.println("4. Cadastrar entregador");
+            System.out.println("5. Listar restaurante");
+            System.out.println("6. Listar entregadores");
+            System.out.println("7. Listar pedidos");
+            System.out.println("0. Voltar");
+            System.out.println("Escolha uma opção: ");
 
-        System.out.println("Digite o endereço: ");
-        String endereco = sc.nextLine();
+            int opcao = Integer.parseInt(sc.nextLine());
 
-        TipoUsuario1 tipoUsuario = TipoUsuario1.CLIENTE;
+            switch(opcao){
+                case 1:
+                    cadastrarCategoria();
+                    break;
+                    case 2:
+                        cadastrarRestaurante();
+                        break;
+                            case 3:
+                                cadastrarProdutoNoRestaurante();
+                                break;
+                            case 4:
+                                cadastrarEntregador();
+                                break;
+                            case 5:
+                                listarRestaurante();
+                                break;
+                            case 6:
+                                listarEntregadores();
+                                break;
+                            case 7:
+                                listarPedidos();
+                                break;
+                            case 0:
+                                voltar = true;
+                                break;
+                            default:
+                                System.out.println("Opção inválida");
+                        }
+                    }
+            }
 
-        Cliente cliente = new Cliente(idUsuario, nome, CPF, email, senha, telefone,tipoUsuario, endereco);
+            public static void cadastrarCategoria(){
+                System.out.println("Digite o nome da categoria: ");
+                String nome = sc.nextLine();
+                Categoria categoria = new Categoria(nome);
 
-        idUsuario++;
-
-        System.out.println("cliente cadastrado com sucesso!");
-        System.out.println("Clientes: "+ cliente);
+                categorias.add(categoria);
+                System.out.println("Categoria adicionada!");
+                System.out.println("Categoria criada: " + categoria.getNome());
     }
 }
+
